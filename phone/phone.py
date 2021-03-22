f6f54e2dce93b9cdef9b73fe81ff83d5f58c25df
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-22 23:22:36 +0100

    moved test.py contents to phone.py and refactored

--- phone/phone.py
+++ phone/phone.py
@@ -3,8 +3,24 @@ import re
 
 connection = sqlite3.connect('db.sqlite3')
 cur = connection.cursor()
+number = cur.execute("SELECT number FROM phone;")
+number_list = number.fetchall()
 
-a = cur.execute("SELECT number from phone;")
-for i in a.fetchall():
-    print(i)
-    new = re.findall("[0-9]", str(i))
+new_list = list()
+for i in number_list:
+    number = i[0]
+    cleaned_number = re.findall("[0-9]+", str(number))
+    item = ''.join(cleaned_number)
+    new_list.append(item)
+
+cur.execute("DELETE FROM phone;")
+connection.commit()
+for new_num in new_list:
+    mobile = cur.execute("SELECT number FROM phone WHERE number=?", (new_num,))
+    mobile_no = mobile.fetchone()
+    if mobile_no is None:
+        cur.execute("INSERT INTO phone (number) VALUES (?)", (new_num,))
+        connection.commit()
+    else:
+        continue
+        
