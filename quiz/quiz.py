60cfbfc53c6033f97f1bc3d26d99e9e2c43185cc
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-22 23:22:59 +0100

    moved code from test.py to quiz.py and refactored

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,18 +1,51 @@
 import csv 
+from time import * 
+import threading
 
-fhand = open('problems.csv')
-reader = csv.reader(fhand)
+try:
+    user_input = str(input('Input the file name you want: '))
+    fhand = open(user_input)
+    reader = csv.reader(fhand)
+except FileNotFoundError:
+    print('-----------File not found--------------')
+    fhand = open('problems.csv')
+    reader = csv.reader(fhand)
+
+
+def countdown():
+    global my_timer
+    
+    my_timer = 5
+    
+    for x in range(5):
+        my_timer = my_timer - 1
+        sleep(1)
+        
+    print("Out of time!")
+    
+countdown_thread = threading.Thread(target=countdown)
+
+countdown_thread.start()
 
 correct = 0
 count = 0
-for row in reader:
-    count += 1
-    print(row[0])
-    user_input = str(input('Input answer: '))
-    if user_input == row[1]:
-        correct += 1
-    else:
-        continue
+while my_timer > 0 :
+    for row in reader:
+        count += 1
+        print(row[0])
+        if my_timer == 0 :
+            break
+        user_input = str(input('Input answer: '))
+        user_input = user_input.lower()
+        user_input = user_input.strip()
+        sleep(1)
+        if my_timer == 0 :
+            break
+        if user_input == row[1]:
+            correct += 1
+            
+        else:
+            continue
 
 print('You got '+ str(correct) + ' answer(s) right')
 print('Number of questions:', count)
