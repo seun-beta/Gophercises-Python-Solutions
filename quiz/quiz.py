2a7227540b3f6fd4b9e4ebfa24f9f2f6e8186a60
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-04-26 18:13:50 +0100

    removed * import, refactored code

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,52 +1,56 @@
-import csv 
-from time import * 
+import csv
+from time import sleep
 import threading
 
+
 try:
-    user_input = str(input('Input the file name you want: '))
+    user_input = str(input('Input your file name: '))
     fhand = open(user_input)
     reader = csv.reader(fhand)
 except FileNotFoundError:
-    print("-----------File not found--------------\n Opening 'problems.csv' ")
+    print("""-----------File not found--------------\n
+            Using 'problems.csv' """)
     fhand = open('problems.csv')
     reader = csv.reader(fhand)
+
     try:
         timer = int(input('Input your timer: '))
-    except:
+    except ValueError:
+        print('Using 30 seconds as the default time')
         timer = 30
 
+
 def countdown():
-    global my_timer
+
     global timer
-    my_timer = timer
-    for _ in range(my_timer):
-        my_timer = my_timer - 1
+    for _ in range(timer):
+        timer = timer - 1
         sleep(1)
-        
+
     print("Out of time!, Press Enter to see your score")
-    
+
+
 countdown_thread = threading.Thread(target=countdown)
 countdown_thread.start()
 
 correct = 0
 count = 0
-while my_timer > 0 :
-    for row in reader:
-        count += 1
-        print(row[0])
-        if my_timer == 0 :
-            break
-        user_input = str(input('Input answer: '))
-        user_input = user_input.lower()
-        user_input = user_input.strip()
-        sleep(1)
-        if my_timer == 0 :
-            break
-        if user_input == row[1]:
-            correct += 1
-            
-        else:
-            continue
 
-print('You got '+ str(correct) + ' answer(s) right')
+for row in reader:
+    count += 1
+    print(row[0])
+    if timer == 0:
+        break
+    user_input = str(input('Input answer: '))
+    user_input = user_input.lower()
+    user_input = user_input.strip()
+
+    if timer == 0:
+        break
+    if user_input == row[1]:
+        correct += 1
+    else:
+        continue
+
+print('You got ' + str(correct) + ' answer(s) right')
 print('Number of questions:', count)
